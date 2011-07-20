ab0aaebd4a2638d86905ad6667ad5f0029872819
Aaron Iles <aaron.iles@gmail.com>
2011-07-20 20:26:47 +1000

    Initial import of project template
    No functionality. Just basic project layout.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name="filemagic",
+    version="0.1",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    author="Aaron Iles",
+    author_email="aaron.iles@gmail.com",
+    url="http://github.com/aliles/filemagic",
+    download_url="http://github.com/aliles/filemagic/downloads",
+    description="File type identification using libmagic",
+    long_description=open('README.rst').read(),
+    license="PSF",
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite = "tests"
+)
