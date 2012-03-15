6ceb5112b56b159c7773eb4e082f03eb57ec3616
Aaron Iles <aaron.iles@gmail.com>
2012-03-14 19:20:25 -0700

    Update trove classifiers for package
    Added operating system and root level python programming language.

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
