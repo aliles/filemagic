72f93b582ab94f357abb2cae4f19488df7bf8da9
Philippe Ombredanne <pombredanne@gmail.com>
2013-09-20 17:19:39 +0200

    Support loading magic1 too

--- magic/api.py
+++ magic/api.py
@@ -9,7 +9,7 @@ import ctypes
 import platform
 import warnings
 
-libname = ctypes.util.find_library('magic')
+libname = ctypes.util.find_library('magic') or ctypes.util.find_library('magic1')
 if not libname:
     if platform.system() == 'SunOS':
         libname = 'libmagic.so'
